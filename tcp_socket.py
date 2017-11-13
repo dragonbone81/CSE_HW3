f6047f55aff41a7618afa3cb3f48e857089027c9
Christian <cvernikoff@gmail.com>
2017-11-13 13:26:31 -0800

    Initial commit

---
+++ tcp_socket.py
@@ -0,0 +1,22 @@
+import socket
+
+
+def tcp_socket_send(ip, port):
+    data = ''
+    tcp_message = "GET / HTTP/1.1\r\nhost: www.example.com\r\n\r\n"
+    tcp_sessions = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_sessions.settimeout(1)
+    tcp_sessions.connect((ip, port))
+    tcp_sessions.send(tcp_message.encode())
+    while True:
+        try:
+            data += tcp_sessions.recv(4096).decode()
+        except socket.timeout:
+            break
+
+    tcp_sessions.close()
+    return data
+
+
+if __name__ == "__main__":
+    print(tcp_socket_send('www.andes.ucmerced.edu', 80))
